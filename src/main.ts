798b916f7b730833a190a959495cce3cef98adda
kulisk <kirillka.kulikov@mail.ru>
2021-08-09 14:36:12 +0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { headers } from './middleware/headers.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(headers);
+  app.enableCors();
+  await app.listen(5000);
 }
 
 bootstrap();
---
+++ src/middleware/headers.middleware.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+
+export function headers(req: Request, res: Response, next: NextFunction) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type',
+  );
+  next();
+}
