7dab347d64cb1a872a76405586a10a99f0f4b14d
kulisk <kirillka.kulikov@mail.ru>
2021-08-26 11:56:23 +0400

    debug

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { headers } from './middleware/headers.middleware';
 
 async function bootstrap() {
+  console.log(process.env.DB_HOST);
   const app = await NestFactory.create(AppModule);
   app.use(headers);
   app.enableCors();
