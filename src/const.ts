dc404589e4c2ad2c1c464ee0e2b04a892d14a0d6
kulisk <kirillka.kulikov@mail.ru>
2021-09-01 12:13:09 +0400

    fix env problems

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const AWS_ACCESS_KEY_ID = 'AKIA4AGUFSORKOZAH3UC';
-export const AWS_SECRET_ACCESS_KEY = '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';
-export const S3_BUCKET_NAME = 'mynotesbusket';
-export const AWS_REGION = 'eu-central-1';
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+export const AWS_REGION = process.env.AWS_REGION;
