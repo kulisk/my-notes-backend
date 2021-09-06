d5246bf1692b007965be4c937851afaaec1970bc
kulisk <kirillka.kulikov@mail.ru>
2021-09-06 09:48:20 +0400

    fix env problems

--- src/images/s3.service.ts
+++ src/images/s3.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { S3 } from 'aws-sdk';
-import {
-  S3_BUCKET_NAME,
-  AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY,
-  AWS_REGION,
-} from '../const';
+import { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } from '../const';
 
 const s3 = new S3({
   accessKeyId: AWS_ACCESS_KEY_ID,
@@ -15,12 +10,16 @@ const s3 = new S3({
 
 @Injectable()
 export class S3Service {
+  constructor() {
+    this.S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+  }
+  S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
   async copy(key: string, newKey: string) {
     await s3
       .copyObject({
-        Bucket: S3_BUCKET_NAME,
+        Bucket: this.S3_BUCKET_NAME,
         Key: newKey,
-        CopySource: encodeURI(`${S3_BUCKET_NAME}/${key}`),
+        CopySource: encodeURI(`${this.S3_BUCKET_NAME}/${key}`),
       })
       .promise();
   }
@@ -28,7 +27,7 @@ export class S3Service {
   async delete(key: string) {
     await s3
       .deleteObject({
-        Bucket: S3_BUCKET_NAME,
+        Bucket: this.S3_BUCKET_NAME,
         Key: key,
       })
       .promise();
